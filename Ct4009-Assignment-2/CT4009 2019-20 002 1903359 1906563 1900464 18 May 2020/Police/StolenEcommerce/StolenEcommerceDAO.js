c259bee465666019f7e847e8edf3b16c834521ac
Nootalie-Nooter <gnatboard@gmail.com>
2020-10-16 09:44:00 +0100

    deleted duplicates

--- Ct4009-Assignment-2/CT4009 2019-20 002 1903359 1906563 1900464 18 May 2020/Police/StolenEcommerce/StolenEcommerceDAO.js
+++
@@ -1,4 +0,0 @@
-//gets the value for the bike id and stores it in a variable
-var bikeID = $('#bikeID').val();
-//passes this variable to StolenEcommerceDAO.php and calls that page
-window.location.href = "StolenEcommerceDAO.php?w1=" + bikeID;
--- Ct4009-Assignment-2/CT4009 2019-20 002 1903359 1906563 1900464 18 May 2020/Police/ViewComparison/ViewComparisonDAO.js
+++
@@ -1,3 +0,0 @@
-alert("In viewComparisonDAO.js")
-var bikeID = $('#bikeID').val();
-window.location.href = "ViewComparisonDAO.php?w1=" + bikeID;
